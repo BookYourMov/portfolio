1f0f0b887d6e1511955c9fae92dbb0643b80c10a
BookYourMov <bookyourmov@gmail.com>
2025-07-12 00:35:48 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,10 +64,11 @@ function App() {
           <Analytics />
         </div>
       )}
-    </>
       {/* Scroll Progress Bar */}
       <motion.div
         className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-purple-500 to-pink-500 z-50 origin-left"
+      />
+    </>
   );
 }
 
